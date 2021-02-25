41989c3f4b71786dd6288e9f22e4edc7342ecc37
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-02-25 08:53:45 +0000

    2021-02-25:08:53:45: generate-with-kotlin is build with npm and ncc

--- generate-with-kotlin/dist/index.js
+++ generate-with-kotlin/dist/index.js
@@ -1579,21 +1579,21 @@ const exec = __webpack_require__(960);
 
 async function run() {
   try {
-    const delimiter = core.getInput('delimeter')
-    const delimeted_arguments = core.getInput('delimeted_arguments')
+    const delimiter = core.getInput('delimiter')
+    const delimited_arguments = core.getInput('delimited_arguments')
     const generator = core.getInput('generator')
     const generated_shell = core.getInput('generated_shell')
 
     core.info(
         "Arguments to index.js: " + [
-          delimiter, delimeted_arguments, generator, generated_shell
+          delimiter, delimited_arguments, generator, generated_shell
         ]
     )
 
     // Generate shell script with generate.sh (uses generator)
     await exec.exec(
         `${__dirname}/../generate.sh`,
-        [generator, delimiter, delimeted_arguments, generated_shell]
+        [generator, delimiter, delimited_arguments, generated_shell]
     );
   } catch (error) {
     core.setFailed(error.message);
