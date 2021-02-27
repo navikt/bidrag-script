e52643e1f5ea26635e205096fbea497658ea8ba2
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-02-27 10:46:24 +0000

    2021-02-27:10:46:24: generate-with-kotlin is build with npm and ncc

--- create-cucumber-shell/dist/index.js
+++ create-cucumber-shell/dist/index.js
@@ -1579,32 +1579,39 @@ const exec = __webpack_require__(960);
 
 async function run() {
   try {
-    const cucumberTag = 'cucumber_tag=' + core.getInput('cucumber_tag')
-    const doNotFail = 'do_not_fail=' + core.getInput('do_not_fail')
-    const mavenGoal = 'maven_goal=' + core.getInput('maven_goal')
-    const user = 'user=' + core.getInput('user')
-
-    const finalShellName = 'final_shell_name=' + core.getInput(
-        'final_shell_name'
-    )
+    const cucumberTag = 'cucumber_tag=' + core.getInput('cucumber_tag');
+    const doNotFail = 'do_not_fail=' + core.getInput('do_not_fail');
+    const githubProjectName = core.getInput('github_project_name');
+    const finalShellName = core.getInput('final_shell_name');
+    const finalShellNameAsMap = 'final_shell_name=' + finalShellName;
+    const mavenGoal = 'maven_goal=' + core.getInput('maven_goal');
+    const user = 'user=' + core.getInput('user');
 
     const optionalMavenGoal = 'optional_maven_goal=' + core.getInput(
         'optional_maven_goal'
-    )
+    );
 
-    const relaticeJsonPath = 'relative_json_path=' + core.getInput(
+    const relativeJsonPath = 'relative_json_path=' + core.getInput(
         'relative_json_path'
-    )
+    );
 
-    var argumentsToShell = [
-      cucumberTag, doNotFail, finalShellName, mavenGoal, optionalMavenGoal,
-      relaticeJsonPath, user
-    ]
+    const argumentsAsMapForKotlinAndForExtraArgsForShell = [
+      cucumberTag + ',' + doNotFail + ',' + finalShellNameAsMap + ','
+      + mavenGoal + ',' + optionalMavenGoal + ',' + relativeJsonPath + ','
+      + user,
+      githubProjectName,
+      finalShellName
+    ];
 
-    console.log("Arguments to shell script: " + argumentsToShell)
+    console.log(
+        "Arguments to shell script: "
+        + argumentsAsMapForKotlinAndForExtraArgsForShell
+    );
 
     // Run createCucumberShell.sh with all arguments (runs createCucumberShell.kts)
-    await exec.exec(`${__dirname}/../createCucumberShell.sh`, argumentsToShell);
+    await exec.exec(`${__dirname}/../createCucumberShell.sh`,
+        argumentsAsMapForKotlinAndForExtraArgsForShell
+    );
   } catch (error) {
     core.setFailed(error.message);
   }
