6e74acfacd09435273e59ef70e7a97b750bd24fa
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2021-02-27 09:42:38 +0000

    2021-02-27:09:42:38: generate-with-kotlin is build with npm and ncc

--- create-cucumber-shell/dist/index.js
+++ create-cucumber-shell/dist/index.js
@@ -1579,17 +1579,29 @@ const exec = __webpack_require__(960);
 
 async function run() {
   try {
-    // Generate shell script with createCucumberShell.sh
+    const cucumberTag = 'cucumber_tag=' + core.getInput('cucumber_tag');
+    const doNotFail = 'do_not_fail=' + core.getInput('do_not_fail');
+    const mavenGoal = 'maven_goal=' + core.getInput('maven_goal');
+    const user = 'user=' + core.getInput('user');
+
+    const finalShellName = 'final_shell_name=' + core.getInput(
+        'final_shell_name'
+    );
+
+    const optionalMavenGoal = 'optional_maven_goal=' + core.getInput(
+        'optional_maven_goal'
+    );
+
+    const relaticeJsonPath = 'relative_json_path=' + core.getInput(
+        'relative_json_path'
+    );
+
+    // Run createCucumberShell.sh with all arguments (runs createCucumberShell.kts)
     await exec.exec(
         `${__dirname}/../createCucumberShell.sh`,
         [
-          'cucumber_tag=' + core.getInput('cucumber_tag'),
-          'do_not_fail=' + core.getInput('do_not_fail'),
-          'final_shell_name=' + core.getInput('final_shell_name'),
-          'maven_goal=' + core.getInput('maven_goal'),
-          'optional_maven_goal=' + core.getInput('optional_maven_goal'),
-          'relative_json_path=' + core.getInput('relative_json_path'),
-          'user=' + core.getInput('user')
+          cucumberTag, doNotFail, finalShellName, mavenGoal, optionalMavenGoal,
+          relaticeJsonPath, user
         ]
     );
   } catch (error) {
